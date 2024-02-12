10ea46f3ec924c6b14fbd174260a0efb480a63f3
Artem <artemm3note@gmail.com>
2024-02-12 20:57:22 +0200

    Extend course info, now component can get any amout of parts

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,18 +1,69 @@
-const App = (props) => {
-  const { notes } = props
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
 
+const Part = (props) => {
+  return (
+    <p>{props.part.name} {props.part.exercises}</p>
+  )
+}
+
+const Content = ({ parts }) => {
+  console.log(parts)
   return (
     <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map(note => 
-          <li key={note.id}>
-            {note.content}
-          </li>
-        )}
-      </ul>
+      {parts.map(part => 
+        <Part key={part.id} part={part} />
+      )}
     </div>
   )
+}
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
+      },
+      {
+        name: 'Extra new part',
+        exercises: 42,
+        id: 5
+      }
+    ]
+  }
+
+  return <Course course={course} />
 }
 
 export default App
