ba0655e61a7c22282492c8e5fffcf0c4b1d78e8d
Artem <artemm3note@gmail.com>
2024-02-14 23:39:11 +0200

    Added db.json file on json-server and then fetched data from server and displays it on page.

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,29 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Persons from './components/Person'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
 
 import axios from 'axios'
 
-axios.get('http://localhost:3001/notes')
-  .then(response => {
-    const notes = response.data
-    console.log(notes)
-  })
-// TODO move request to App.jsx
-
-
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newSearch, setNewSearch] = useState('')
   const [shownPersons, setShownPersons] = useState(persons)
+
+  const hook = () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        const people = response.data
+        console.log(people)
+        setPersons(people)
+        setShownPersons(people)
+      })
+  }
+
+  useEffect(hook, [])
 
   const addPerson = (e) => {
     e.preventDefault()
